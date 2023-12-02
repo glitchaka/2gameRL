fb991bf5534a6b9a3e5af230392ae2afc97114db
glitchaka <aaron.ulloagon@gmail.com>
2023-12-02 17:22:50 -0300

    Creando Canvas, y designando teclado

--- com/buttclapdev/main/graphics/DrawSurface.java
+++ com/buttclapdev/main/graphics/DrawSurface.java
@@ -1,6 +1,29 @@
 package com.buttclapdev.main.graphics;
 
-import java.awt.*;
+import com.buttclapdev.main.control.Keyboard;
 
-public class DrawSurface extends Canvas {
+import java.awt.*;
+import java.io.Serializable;
+
+public class DrawSurface extends Canvas implements Serializable {
+    //atributos
+    private static final long serialVersionUID = -2284879212465893870L;
+
+    private  int width;
+    private int height;
+
+    private Keyboard keyboard;
+
+    //constructores
+    public DrawSurface(final int width, final int height){
+        this.width = width;
+        this.height = height;
+
+        keyboard = new Keyboard();
+
+        setIgnoreRepaint(true);
+        setPreferredSize(new Dimension(width, height));
+        addKeyListener(keyboard);
+    }
+
 }
