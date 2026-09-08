b40a992391f59f9edb655ce6b37282a8795d200f
glitchaka <82622846+glitchaka@users.noreply.github.com>
2026-09-08 13:48:56 -0300

    2.0.9: make Windows smoke project verify BoxCollider and components

--- src/test/java/com/buttclapdev/twogamerl/ComponentSmokeProjectCreator.java
+++ src/test/java/com/buttclapdev/twogamerl/ComponentSmokeProjectCreator.java
@@ -29,9 +29,26 @@ public final class ComponentSmokeProjectCreator {
         Level level = project.getLevels().get("level-1");
 
         EntityDef player = level.entity("player");
+        player.physicsLayer = "Player";
         player.components.add(ComponentDef.preset("Health"));
+        player.script = "on start\n  log \"PLAYER_START_OK\"\nend\n" +
+                "on update\n  velocity 6 0\nend\n" +
+                "on collision\n  log \"BOXCOLLIDER_OK\"\nend\n";
+
+        EntityDef blocker = new EntityDef("blocker-smoke", "BlockerSmoke", 4, 2);
+        blocker.physicsLayer = "World";
+        blocker.components.add(ComponentDef.preset("BoxCollider2D"));
+        blocker.script = "on collision\n  log \"STATIC_COLLIDER_OK\"\nend\n";
+        level.entities.add(blocker);
+
+        EntityDef failSentinel = new EntityDef("fail-sentinel", "FailSentinel", 6, 2);
+        failSentinel.width = 1; failSentinel.height = 1; failSentinel.physicsLayer = "Trigger";
+        failSentinel.components.add(ComponentDef.preset("Trigger"));
+        failSentinel.script = "on trigger\n  log \"BOXCOLLIDER_FAILED_PASSTHROUGH\"\nend\n";
+        level.entities.add(failSentinel);
 
         EntityDef trigger = new EntityDef("trigger-smoke", "TriggerSmoke", 2, 2);
+        trigger.physicsLayer = "Trigger";
         ComponentDef triggerComponent = ComponentDef.preset("Trigger");
         triggerComponent.properties.put("once", "true");
         trigger.components.add(triggerComponent);
@@ -39,20 +56,21 @@ public final class ComponentSmokeProjectCreator {
         level.entities.add(trigger);
 
         EntityDef damage = new EntityDef("damage-smoke", "DamageSmoke", 2, 2);
-        damage.width = .4;
-        damage.height = .4;
+        damage.width = .4; damage.height = .4; damage.physicsLayer = "Enemy";
         ComponentDef damageComponent = ComponentDef.preset("DamageOnContact");
         damageComponent.properties.put("damage", "10");
         damage.components.add(damageComponent);
         level.entities.add(damage);
 
-        EntityDef rigid = new EntityDef("rigid-smoke", "RigidSmoke", 5, 12.5);
+        EntityDef rigid = new EntityDef("rigid-smoke", "RigidSmoke", 8, 12.5);
+        rigid.physicsLayer = "Default";
         rigid.components.add(ComponentDef.preset("Rigidbody2D"));
         rigid.components.add(ComponentDef.preset("BoxCollider2D"));
         rigid.script = "on collision\n  log \"RIGIDBODY_OK\"\nend\n";
         level.entities.add(rigid);
 
         EntityDef patrol = new EntityDef("patrol-smoke", "PatrolSmoke", 18, 5);
+        patrol.physicsLayer = "Enemy";
         ComponentDef patrolComponent = ComponentDef.preset("Patrol");
         patrolComponent.properties.put("distance", "8");
         patrolComponent.properties.put("speed", "6");
@@ -61,39 +79,34 @@ public final class ComponentSmokeProjectCreator {
         patrol.script = "on collision\n  log \"PATROL_COLLIDER_OK\"\nend\n";
         level.entities.add(patrol);
 
-        EntityDef blocker = new EntityDef("blocker-smoke", "BlockerSmoke", 20.2, 5);
-        blocker.components.add(ComponentDef.preset("BoxCollider2D"));
-        level.entities.add(blocker);
+        EntityDef patrolBlocker = new EntityDef("patrol-blocker", "PatrolBlocker", 20.2, 5);
+        patrolBlocker.physicsLayer = "World";
+        patrolBlocker.components.add(ComponentDef.preset("BoxCollider2D"));
+        level.entities.add(patrolBlocker);
         return project;
     }
 
     private static GameProject portalProject() {
         GameProject project = GameProject.createDefault();
         project.setTitle("2gameRL-Portal-Smoke");
-        project.getMenus().clear();
-        project.setStartMenu("");
+        project.getMenus().clear(); project.setStartMenu("");
         Level source = project.getLevels().get("level-1");
 
         EntityDef portal = new EntityDef("portal-smoke", "PortalSmoke", 2, 2);
+        portal.physicsLayer = "Trigger";
         ComponentDef portalComponent = ComponentDef.preset("ScenePortal");
         portalComponent.properties.put("targetScene", "portal-target");
         portalComponent.properties.put("targetX", "7");
         portalComponent.properties.put("targetY", "8");
-        portal.components.add(portalComponent);
-        source.entities.add(portal);
+        portal.components.add(portalComponent); source.entities.add(portal);
 
         Level target = new Level("portal-target", "Portal Target", 24, 16);
-        for (int y = 0; y < target.height; y++) {
-            for (int x = 0; x < target.width; x++) {
-                target.set(x, y, x == 0 || y == 0 || x == target.width - 1 || y == target.height - 1 ? 1 : 0);
-            }
-        }
+        for (int y=0;y<target.height;y++) for (int x=0;x<target.width;x++) target.set(x,y,x==0||y==0||x==target.width-1||y==target.height-1?1:0);
         EntityDef player = new EntityDef("portal-player", "PortalPlayer", 2, 2);
-        player.components.add(ComponentDef.preset("PlayerController"));
-        player.components.add(ComponentDef.preset("BoxCollider2D"));
+        player.physicsLayer = "Player"; player.renderLayer = "Personajes";
+        player.components.add(ComponentDef.preset("PlayerController")); player.components.add(ComponentDef.preset("BoxCollider2D"));
         player.script = "on start\n  log \"PORTAL_TARGET_OK\"\nend\n";
-        target.entities.add(player);
-        project.getLevels().put(target.id, target);
+        target.entities.add(player); project.getLevels().put(target.id,target);
         return project;
     }
 }
